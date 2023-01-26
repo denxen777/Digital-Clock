f85bbeaee27e8a334ed478c6d94429a10b5662c6
Аверьянов Дмитрий Викторович <dvaverjanov@sever.medved-holding.com>
2023-01-26 18:11:45 +0700

    Refactor

--- src/script.js
+++ src/script.js
@@ -1,30 +1,8 @@
 import './style.css';
 
+const addZero = num => +num < 10 ? '0' + +num : num;
 
-/* const curDate = new Date();
-const date = curDate.toLocaleTimeString('en-US', { weekday: 'short' })
-  .split(/\W/).map((v, i) => (i !== 0) ? v : v.toUpperCase())
-  .filter((_, i) => i !== 3);
-
-const elems = document.querySelectorAll('.container_date span');
-
-elems.forEach((e, i) => {
-  if (i !== 1 && i !== 2) {
-    e.textContent = date[i];
-  } else {
-    const span = document.createElement('span');
-    span.textContent = ':';
-    span.classList.add('hidden');
-    e.textContent = addZero(date[i]);
-    e.appendChild(span);
-  }
-}) */
-
-function addZero(num) {
-  return (+num < 10) ? '0' + +num : num;
-}
-
-function showDate() {
+function setDate() {
   const curDate = new Date();
   const date = curDate.toLocaleTimeString('en-US', { weekday: 'short' })
     .split(/\W/).map((v, i) => (i !== 0) ? v : v.toUpperCase())
@@ -34,24 +12,32 @@ function showDate() {
   elems.forEach((e, i) => {
     if (i !== 1 && i !== 2) {
       e.textContent = date[i];
-    } else {
-      const span = document.createElement('span');
-      span.textContent = ':';
-      span.classList.add('hidden');
-      e.textContent = addZero(date[i]);
-      e.appendChild(span);
+      return;
     }
+    const span = document.createElement('span');
+    span.textContent = ':';
+    span.classList.add('hidden');
+    e.textContent = addZero(date[i]);
+    e.appendChild(span);
   })
 }
 
-async function fn() {
+async function displayDate() {
   const response = await new Promise(resolve => {
     let timerId = setTimeout(() => {
-      resolve(showDate);
-      timerId = setTimeout(fn, 0);
-    }, 0);
+      resolve(setDate);
+      timerId = setTimeout(displayDate, 500);
+    }, 500);
   })
 
   return response();
 }
-// fn();
+
+displayDate();
+
+
+
+
+
+
+
